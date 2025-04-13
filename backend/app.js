5f3179bf6d4622c6cae9c60d0fe2a010d6f25aa8
carol <carol.lobo2005@gail.com>
2025-04-13 21:22:56 +0530

    Sockets connected

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const indexRouter=require('./routes/index');
 const expressSession=require("express-session");
 const flash=require('connect-flash');
 const { log } = require('console');
-const io=require('socket.io')(5000,{cors:{origin:'*'}}); 
+
 const cors = require("cors")
 
 
@@ -45,6 +45,18 @@ app.use('/owners',ownersRouter);
 app.use('/users',userRouter);
 app.use('/products',productsRouter);
 
+const http = require('http');
+const server = http.createServer(app);
+
+// Socket.io setup
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
 io.on('connection', (socket) => {
     console.log('A user connected', socket.id);
@@ -66,15 +78,18 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        socket.rooms.forEach(roomId => {
-            socket.broadcast.to(roomId).emit('left', 'A user');
-        });
+        if (socket.rooms) {
+            for (const roomId of socket.rooms) {
+                if (roomId !== socket.id) { // Skip the default room (socket ID)
+                    socket.broadcast.to(roomId).emit('left', 'A user');
+                }
+            }
+        }
         console.log('A user disconnected:', socket.id);
     });
 });
-app.listen(3000,()=>{
-    console.log("Server running on port 3000");
-    
+server.listen(3000, () => {
+    console.log("Server + Socket.IO running on port 3000");
 });
 
 
